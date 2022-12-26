d5aaef028ff13af50d4fded6a6eeaf02ce997503
KimHyunLo <leeas016@gmail.com>
2022-12-26 10:05:34 +0900

    step 1

---
+++ inherited_notifier/lib/main.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Flutter Demo',
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+    ),
+    debugShowCheckedModeBanner: false,
+    home: const HomeScreen(),
+  ));
+}
+
+class SliderData extends ChangeNotifier {
+  double _value = 0;
+
+  double get value => _value;
+
+  set value(double newValue) {
+    if (newValue != _value) {
+      _value = newValue;
+      notifyListeners();
+    }
+  }
+}
+
+final sliderData = SliderData();
+
+class SliderInheritedNotifier extends InheritedNotifier<SliderData> {
+  const SliderInheritedNotifier({
+    Key? key,
+    required Widget child,
+    required SliderData sliderData,
+  }) : super(
+          key: key,
+          child: child,
+          notifier: sliderData,
+        );
+
+  static double of(BuildContext context) =>
+      context.dependOnInheritedWidgetOfExactType<SliderInheritedNotifier>()?.notifier?.value ?? 0;
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Screen'),
+      ),
+      body: SliderInheritedNotifier(
+        sliderData: sliderData,
+        child: Builder(
+          builder: (context) {
+            return Column(
+              children: [
+                Slider(
+                  value: SliderInheritedNotifier.of(context),
+                  onChanged: (value) {
+                    sliderData.value = value;
+                  },
+                ),
+                Row(
+                  children: [
+                    Opacity(
+                      opacity: SliderInheritedNotifier.of(context),
+                      child: Container(
+                        color: Colors.yellow,
+                        height: 200,
+                      ),
+                    ),
+                    Opacity(
+                      opacity: SliderInheritedNotifier.of(context),
+                      child: Container(
+                        color: Colors.blue,
+                        height: 200,
+                      ),
+                    ),
+                  ].expandEqually().toList(),
+                )
+              ],
+            );
+          }
+        ),
+      ),
+    );
+  }
+}
+
+extension ExpandEqually on Iterable<Widget> {
+  Iterable<Widget> expandEqually() => map(
+        (w) => Expanded(child: w),
+      );
+}
