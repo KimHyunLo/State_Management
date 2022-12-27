99df79916208af42d4dcca8f8f84116bead4cc8a
KimHyunLo <leeas016@gmail.com>
2022-12-27 13:45:21 +0900

    step 6

--- real_bloc/lib/main.dart
+++ real_bloc/lib/main.dart
@@ -1,16 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:real_bloc/apis/login_api.dart';
-import 'package:real_bloc/apis/notes_api.dart';
-import 'package:real_bloc/bloc/actions.dart';
-import 'package:real_bloc/bloc/app_bloc.dart';
-import 'package:real_bloc/bloc/app_state.dart';
-import 'package:real_bloc/dialogs/generic_dialog.dart';
-import 'package:real_bloc/dialogs/loading_screen.dart';
-import 'package:real_bloc/models.dart';
-import 'package:real_bloc/strings.dart';
-import 'package:real_bloc/views/iterable_list_view.dart';
-import 'package:real_bloc/views/login_view.dart';
+import 'package:real_bloc/views/home_page.dart';
 
 void main() {
   runApp(
@@ -20,75 +9,7 @@ void main() {
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
-      home: const HomeScreen(),
+      home: const HomePage(),
     ),
   );
-}
-
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => AppBloc(
-        loginApi: LoginApi(),
-        notesApi: NotesApi(),
-        acceptedLoginHandle: const LoginHandle.fooBar(),
-      ),
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text(homePage),
-        ),
-        body: BlocConsumer<AppBloc, AppState>(
-          listener: (context, appState) {
-            if (appState.isLoading) {
-              LoadingScreen.instance().show(
-                context: context,
-                text: pleaseWait,
-              );
-            } else {
-              LoadingScreen.instance().hide();
-            }
-
-            final loginError = appState.loginError;
-            if (loginError != null) {
-              showGenericDialog<bool>(
-                context: context,
-                title: loginErrorDialogTitle,
-                content: loginErrorDialogContent,
-                optionsBuilder: () => {ok: true},
-              );
-            }
-
-            if (appState.isLoading == false &&
-                appState.loginError == null &&
-                appState.loginHandle == const LoginHandle.fooBar() &&
-                appState.fetchedNotes == null) {
-              context.read<AppBloc>().add(
-                    const LoadNotesAction(),
-                  );
-            }
-          },
-          builder: (context, appState) {
-            final notes = appState.fetchedNotes;
-            if (notes == null) {
-              return LoginView(
-                onLoginTapped: (email, password) {
-                  context.read<AppBloc>().add(
-                        LoginAction(
-                          email: email,
-                          password: password,
-                        ),
-                      );
-                },
-              );
-            } else {
-              return notes.toListView();
-            }
-          },
-        ),
-      ),
-    );
-  }
 }
