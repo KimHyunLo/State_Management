db409a7a2d17f0d8ce6628c14a0b09be11d1a033
KimHyunLo <leeas016@gmail.com>
2022-12-27 14:19:02 +0900

    step 7

---
+++ real_bloc/test/app_bloc_test.dart
@@ -0,0 +1,116 @@
+import 'dart:typed_data';
+
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:real_bloc/bloc/app_bloc.dart';
+import 'package:real_bloc/bloc/app_state.dart';
+import 'package:real_bloc/bloc/bloc_events.dart';
+
+extension ToList on String {
+  Uint8List toUint8List() => Uint8List.fromList(codeUnits);
+}
+
+final text1Data = 'Foo'.toUint8List();
+final text2Data = 'Bar'.toUint8List();
+
+enum Errors { dummy }
+
+void main() {
+  blocTest(
+    'Initial state of the bloc should be empty',
+    build: () => AppBloc(
+      urls: [],
+    ),
+    verify: (appBloc) => expect(
+      appBloc.state,
+      const AppState.empty(),
+    ),
+  );
+
+  blocTest(
+    'Test the ability to load a URL',
+    build: () => AppBloc(
+      urls: [],
+      urlPicker: (_) => '',
+      urlLoader: (_) => Future.value(text1Data),
+    ),
+    act: (appBloc) => appBloc.add(
+      const LoadNextUrlEvent(),
+    ),
+    expect: () => [
+      const AppState(
+        isLoading: true,
+        data: null,
+        error: null,
+      ),
+      AppState(
+        isLoading: false,
+        data: text1Data,
+        error: null,
+      ),
+    ],
+  );
+
+  blocTest(
+    'Throw an error in url loader and catch it',
+    build: () => AppBloc(
+      urls: [],
+      urlPicker: (_) => '',
+      urlLoader: (_) => Future.error(Errors.dummy),
+    ),
+    act: (appBloc) => appBloc.add(
+      const LoadNextUrlEvent(),
+    ),
+    expect: () => [
+      const AppState(
+        isLoading: true,
+        data: null,
+        error: null,
+      ),
+      const AppState(
+        isLoading: false,
+        data: null,
+        error: Errors.dummy,
+      ),
+    ],
+  );
+
+  blocTest(
+    'Test the ability to load more than one URL',
+    build: () => AppBloc(
+      urls: [],
+      urlPicker: (_) => '',
+      urlLoader: (_) => Future.value(text2Data),
+    ),
+    act: (appBloc) {
+      appBloc.add(
+        const LoadNextUrlEvent(),
+      );
+      appBloc.add(
+        const LoadNextUrlEvent(),
+      );
+    },
+    expect: () => [
+      const AppState(
+        isLoading: true,
+        data: null,
+        error: null,
+      ),
+      AppState(
+        isLoading: false,
+        data: text2Data,
+        error: null,
+      ),
+      const AppState(
+        isLoading: true,
+        data: null,
+        error: null,
+      ),
+      AppState(
+        isLoading: false,
+        data: text2Data,
+        error: null,
+      ),
+    ],
+  );
+}
